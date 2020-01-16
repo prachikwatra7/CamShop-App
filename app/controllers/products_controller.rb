7ca11b4ddcf00ef1cbf89000ef0aa803efaebcd2
prachikwatra7 <prachikwatra7@gmail.com>
2020-01-16 15:27:22 +0000

    Add edit, update, show and index listing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,15 @@
 class ProductsController < ApplicationController
+    
+    def index
+        @products = Product.all    
+    end
+    
     def new
       @product = Product.new
+    end
+    
+    def edit
+      @product = Product.find(params[:id]) 
     end
     
     def create
@@ -11,6 +20,16 @@ class ProductsController < ApplicationController
             redirect_to product_path(@product)
         else
             render 'new'
+        end
+    end
+    
+    def update
+       @product = Product.find(params[:id]) 
+        if @product.update(product_params)
+           flash[:notice] = "Product was successfully updated"
+           redirect_to product_path(@product)
+        else
+            render 'edit'
         end
     end
     
